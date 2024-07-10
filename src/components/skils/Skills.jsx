f3cd2572de396705a17acf0e6f7dd93988442ad6
diego072117 <parracalderond9@gmail.com>
2024-07-10 12:12:48 -0500

    fix(duplicate): duplicate skill.

--- src/components/skils/Skills.jsx
+++ src/components/skils/Skills.jsx
@@ -109,7 +109,7 @@ export const Skills = () => {
               <article className="experience__details">
                 <BsPatchCheckFill className="experience__details-icon" />
                 <div>
-                  <h4>Astro</h4>
+                  <h4>Bootstrap</h4>
                 </div>
               </article>
               <article className="experience__details">
@@ -145,61 +145,6 @@ export const Skills = () => {
             </div>
           </div>
         </div>
-
-        {/* <div className="experience__backend">
-          <h3>Backend Development</h3>
-          <div className="experience__content">
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>PHP</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Laravel</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>Java</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>SpringBoot</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>NodeJs</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4>PostgreSql</h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-            <article className="experience__details">
-              <BsPatchCheckFill className="experience__details-icon" />
-              <div>
-                <h4></h4>
-                <small className="text-light">Experienced</small>
-              </div>
-            </article>
-          </div>
-        </div> */}
       </div>
     </section>
   );
